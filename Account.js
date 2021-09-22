d3a42bd19dc87281509930a79c6db7f51b549d62
Donatas <donkuprys@gmail.com>
2021-09-22 19:24:31 +0300

    add money to account

--- Account.js
+++ Account.js
@@ -12,20 +12,18 @@ Account.create = async (connection, userId) => {
 }
 
 Account.AdditionByAccountId = async (connection, accountId, cash) => {
-    let sql2 = 'UPDATE `accounts`\
+    let sql = 'UPDATE `accounts`\
                  SET `balance` = `balance` +"'+ cash + '"\
                   WHERE `accounts`.`id` ='+ accountId;
 
-    [rows] = await connection.execute(sql2);
-    // console.log(`${cash} pinigu buvo sekmingai prideti i saskaita`);
-    // return true;
-    if (!!rows.affectedRows) {
+    [rows] = await connection.execute(sql);
+    if (rows.affectedRows) {
         console.log(`${cash} pinigu buvo sekmingai prideti i saskaita`);
     } else {
         console.log(`pinigu prideti nepavyko!`);
     }
 
-    return !!rows.affectedRows;
+    return rows.affectedRows;
 }
 
 
--- index.js
+++ index.js
@@ -17,13 +17,21 @@ app.init = async () => {
 
     const user1 = await User.create(conn, 'Vardenis', 'Pavardenis');
     console.log(user1);
+    const user2 = await User.create(conn, 'Banginis', 'Vandeninis');
+    console.log(user2);
+    const user3 = await User.create(conn, 'Mike', 'Pukuotas');
+    console.log(user3);
 
-    let accountUser = await Account.create(conn, 1)
-    console.log(accountUser);
+    // let accountUser = await Account.create(conn, 1)
+    // console.log(accountUser);
 
 
-    let addToAccount = await Account.AdditionByAccountId(conn, 1, 500)
-    console.log(addToAccount);
+    let addToAccount1 = await Account.AdditionByAccountId(conn, 1, 500)
+    console.log(addToAccount1);
+    let addToAccount2 = await Account.AdditionByAccountId(conn, 2, 5500)
+    console.log(addToAccount2);
+    let addToAccount3 = await Account.AdditionByAccountId(conn, 3, 1500)
+    console.log(addToAccount3);
 
 
 }
